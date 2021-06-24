d79c34af328a32bb9c81a28505038c8ec33c9e4a
DINESHKUMAR <dk1981999@gmail.com>
2021-06-24 11:07:04 +0530

    fixed http methods

--- src/app/sharedFolder/storage.service.ts
+++ src/app/sharedFolder/storage.service.ts
@@ -7,18 +7,44 @@ import { StudentService } from './student.service';
   providedIn: 'root'
 })
 export class StorageService {
-
+  url: string = 'https://student-management-syste-5e87d-default-rtdb.firebaseio.com/students/';
+  urlDev: string = 'https://development-student-default-rtdb.firebaseio.com/students/';
   constructor(private http: HttpClient, private studentService: StudentService) { }
 
-  storeStudents(){
-    const students = this.studentService.getStudents();
-    this.http.put('https://student-management-syste-5e87d-default-rtdb.firebaseio.com/students.json',students).subscribe();
+  studentPost(student: Student) {
+    student.id = Math.floor(1000 + Math.random() * 9000);
+    const url = this.urlDev + '.json';
+    this.http.post(url, student)
+      .subscribe((response) => {
+        if (response) {
+          this.studentGet();
+        }
+      });
   }
 
-  fetchStudents(){ 
-    return this.http.get<Student[]>('https://student-management-syste-5e87d-default-rtdb.firebaseio.com/students.json')
-    .subscribe((students)=>{
-      this.studentService.setStudents(students);
-    })
+  studentGet() {
+    const url = this.urlDev + '.json';
+    return this.http.get<Student[]>(url)
+      .subscribe((students) => {
+        this.studentService.setStudents(students);
+      })
+  }
+
+  studentPut(id: string, student: Student) {
+    const url = this.urlDev + id + '.json';
+    this.http.put(url, student, {
+    }).subscribe((response) => {
+      if (response) {
+        this.studentGet();
+      }
+    });
+  }
+
+  studentDelete(id: string) {
+    const url = this.urlDev + id + '.json';
+    this.http.delete(url)
+      .subscribe(() => {
+        this.studentGet();
+      });
   }
 }
